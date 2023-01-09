acb4d3cb1fc2376894cad706120d14ceb643be56
ylose <heungeob1003@gmail.com>
2023-01-09 13:39:09 +0900

    할 일 목록 리스트의 텍스트, 삭제 버튼 생성

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,14 @@ void main() {
   runApp(MyApp());
 }
 
+// 할 일 클래스
+class Todo {
+  bool isDone = false;
+  String title;
+
+  Todo(this.title);
+}
+
 class MyApp extends StatelessWidget {
 
   @override
@@ -25,11 +33,27 @@ class TodoListPage extends StatefulWidget {
 
 class _TodoListPageState extends State<TodoListPage> {
   // 할 일 목록을 저장할 리스트
-  final _items = [];
+  final _items = <Todo>[]; // Todo 객체 리스트
 
   // 할 일 문자열 조작을 위한 컨트롤러
   var _todoController = TextEditingController();
 
+  Widget _buildItemWidget(Todo todo) {
+    return ListTile(
+      onTap:() {}, // Todo : 클릭 시 완료/취소되도록 수정
+      title:Text(
+        todo.title, // 할 일
+        style:todo.isDone ? TextStyle( // 완료일 때는 스타일 적용
+          decoration:TextDecoration.lineThrough, // 취소선
+          fontStyle: FontStyle.italic, // 이탤릭체
+        ) : null, // 그 외에는 아무 스타일 적용 안함
+      ),
+      trailing: IconButton( // 뒤에는 쓰레기통 아이콘을 배치
+        icon:Icon(Icons.delete_forever),
+        onPressed:(){}, //Todo : 쓰레기통 클릭 시 삭제되도록 수정
+      )
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
