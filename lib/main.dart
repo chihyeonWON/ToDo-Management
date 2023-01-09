b27cdb5b26726bf02f0c838960890aec82787c8f
ylose <heungeob1003@gmail.com>
2023-01-09 13:05:46 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'할 일 관리',
+      theme:ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+        home:TodoListPage(),
+    );
+  }
+}
+
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('남은 할 일'),
+      ),
+    );
+  }
+}
+
+
+
+
+
