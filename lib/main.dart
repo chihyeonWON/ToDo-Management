2c696fa8e09736864fc101025bf195d9eb26adb7
ylose <heungeob1003@gmail.com>
2023-01-10 12:58:26 +0900

    할 일 완료/미완료 메서드 생성

--- lib/main.dart
+++ lib/main.dart
@@ -70,6 +70,13 @@ class _TodoListPageState extends State<TodoListPage> {
     });
   }
 
+  // 할 일 완료/미완료 메서드
+  void _toggleTodo(Todo todo) {
+    setState((){
+      todo.isDone = !todo.isDone;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
