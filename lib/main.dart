0691cf16dfe0873b5999680a9ff91511f28cfc2f
ylose <heungeob1003@gmail.com>
2023-01-11 10:23:49 +0900

    Firestore 라이브러리 import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; // Firestore 라이브러리
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
